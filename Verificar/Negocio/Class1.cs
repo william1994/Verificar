b3d23b651a8a3a8a124e18f72de6d4129260643e
denis lopez <lo0706032020@gmail.com>
2021-02-27 18:27:54 -0600

    clase

--- Verificar/Negocio/Class1.cs
+++ Verificar/Negocio/Class1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verificar.DT;
 
 namespace Verificar.Negocio
 {
     class Class1
     {
+        Class cls = new Class();
     }
 }
