78a40857aaa7e893b38b562f1dc3854a66f837d8
William Diaz <williamefrain1994@gmail.com>
2021-02-27 18:30:20 -0600

    Solve

--- Verificar/DT/Class.cs
+++ Verificar/DT/Class.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Verificar.Negocio;
+
 
 namespace Verificar.DT
 {
     class Class
     {
-        Class1 cls1 = new Class1();
+      //  Class1 cls1 = new Class1();
     }
 }
